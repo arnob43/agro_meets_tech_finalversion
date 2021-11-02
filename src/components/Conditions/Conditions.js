fce8bb81264a3a00215e23da8d1a8f59adc8a4de
Shilajit Datta <arnobdatta33@gmail.com>
2021-11-02 22:19:25 +0100

    changes in description and stringify to find the parameter. referring to mozaffar

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './table.css';
+
 const Conditions = (props) => {
    return (
        <div>
+       {/* check here to find out the different parameter names */}
+       {JSON.stringify(props.responseObj)}
            {props.responseObj.cod === 200 ?
+           
             <div>  
-                   <p>It is currently {Math.round((props.responseObj.main.temp)-273.15)} degrees out with {props.responseObj.weather[0].description}.</p>
+                   <p>It is currently {Math.round((props.responseObj.main.temp)-273.15)} degrees out with {props.responseObj.weather[0].description} in {(props.responseObj.name)}.</p>
                    
                  <table>    
                
@@ -14,8 +18,8 @@ const Conditions = (props) => {
                    <th>Humidity</th>
                    <th>Temperature</th>
                    <th>Pressure</th>
-                   {/*API problem so aborting this
-                    <th>Windspeed</th> */}
+                   {/* API problem so aborting this */}
+                    <th>Windspeed</th>
                    <th>Type</th>
                    </tr>
                    
@@ -24,7 +28,7 @@ const Conditions = (props) => {
                     <td>{(props.responseObj.main.humidity)}</td>
                     <td>{Math.round((props.responseObj.main.temp)-273.15)}</td>
                     <td>{(props.responseObj.main.pressure)}</td>
-                    {/* <td>{(props.responseObj.main.speed)}</td> */} 
+                    <td>{(props.responseObj.wind.speed)}</td> 
                     <td>{(props.responseObj.weather[0].description)}
                     </td>
                     </tr>
@@ -34,7 +38,7 @@ const Conditions = (props) => {
                     <td>{(props.responseObj.main.humidity)}</td>
                     <td>{Math.round((props.responseObj.main.temp)-273.15)}</td>
                     <td>{(props.responseObj.main.pressure)}</td>
-                    {/* <td>{(props.responseObj.main.windspeed)}</td> */}
+                    <td>{(props.responseObj.wind.speed)}</td>
                     <td>{(props.responseObj.weather[0].description)}
                     </td>
                     </tr>
@@ -44,7 +48,7 @@ const Conditions = (props) => {
                     <td>{(props.responseObj.main.humidity)}</td>
                     <td>{Math.round((props.responseObj.main.temp)-273.15)}</td>
                     <td>{(props.responseObj.main.pressure)}</td>
-                    {/* <td>{(props.responseObj.main.speed)}</td> */}
+                    <td>{(props.responseObj.wind.speed)}</td>
                     <td>{(props.responseObj.weather[0].description)}
                     </td>
                     </tr>
@@ -54,7 +58,7 @@ const Conditions = (props) => {
                     <td>{(props.responseObj.main.humidity)}</td>
                     <td>{Math.round((props.responseObj.main.temp)-273.15)}</td>
                     <td>{(props.responseObj.main.pressure)}</td>
-                    {/* <td>{(props.responseObj.main.speed)}</td> */}
+                    <td>{(props.responseObj.wind.speed)}</td>
                     <td>{(props.responseObj.weather[0].description)}
                     </td>
                     </tr>
@@ -64,7 +68,7 @@ const Conditions = (props) => {
                     <td>{(props.responseObj.main.humidity)}</td>
                     <td>{Math.round((props.responseObj.main.temp)-273.15)}</td>
                     <td>{(props.responseObj.main.pressure)}</td>
-                    {/* <td>{(props.responseObj.main.speed)}</td> */}
+                    <td>{(props.responseObj.wind.speed)}</td>
                     <td>{(props.responseObj.weather[0].description)}
                     </td>
                     </tr>
